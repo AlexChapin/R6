06de26246f777f22e7a82741510bb57526b17b33
AlexChapin <Achapin@nbinteractive.com>
2025-09-18 16:48:39 -0400

    Add ENES 100 Library / Connect to System

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,5 +1,20 @@
+#include "Enes100.h"
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+bool connecttoENES = False;
+  if (connecttoENES){
+    int wifiModuleTX = 0;
+    int wifiModuleRX = 0;
+    int roomNumber = 1116;
+    int markerId = 0;
+    Enes100.begin("R6", DATA, markerId, roomNumber, wifiModuleTX, wifiModuleRX);
+    Enes100.print("Connected to Vision? " + Enes100.isConnected());
+}
+
+
+
 
 }
 
